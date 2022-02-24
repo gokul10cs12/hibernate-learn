eb302ecbec9053c402af56b20ec88dfc54d4d626
gokulnb <gokul10cs12@gmail.com>
2022-02-24 20:59:46 +0530

    first commit

---
+++ src/main/java/com/example/hibernatelearn/HibernateLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernatelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateLearnApplication.class, args);
+    }
+
+}

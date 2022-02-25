a54f82debea7fc8cacef8375ceae2817373e761e
gokul.nb <gokul.nb@netwitness.com>
2022-02-25 18:42:25 +0530

    model class constraints changes.

--- src/main/java/com/example/hibernatelearn/HibernateLearnApplication.java
+++ src/main/java/com/example/hibernatelearn/HibernateLearnApplication.java
@@ -1,13 +1,28 @@
 package com.example.hibernatelearn;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateLearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HibernateLearnApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student gokul =new Student(
+                    "Gokul",
+                    "N.B",
+                    "abc@abc.com",
+                    27
+            );
+            studentRepository.save(gokul);
+        };
     }
 
 }
--- src/main/java/com/example/hibernatelearn/Student.java
+++ src/main/java/com/example/hibernatelearn/Student.java
@@ -1,10 +1,18 @@
 package com.example.hibernatelearn;
 
+import org.hibernate.annotations.ManyToAny;
+
 import javax.persistence.*;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity(name="Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        }
+)
 public class Student {
     @Id
     @SequenceGenerator(
@@ -17,17 +25,44 @@ public class Student {
             generator = "student_sequence"
     )
     private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String email;
+
+    @Column(
+            name = "age",
+            nullable = false,
+            unique = true
+    )
     private Integer age;
 
-    public Student(Long id, String firstName, String lastName, String email, Integer age) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email, Integer age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.age = age;
+    }
+
+    public Student() {
+
     }
 
     public void setId(Long id) {

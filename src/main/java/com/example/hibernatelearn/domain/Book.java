fdfd1a3ad65b9189bfac71c1ab58dbf3324f92a2
gokul.nb <gokul.nb@netwitness.com>
2022-03-16 20:17:02 +0530

    hash changes

---
+++ src/main/java/com/example/hibernatelearn/domain/Book.java
@@ -0,0 +1,86 @@
+package com.example.hibernatelearn.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+
+/*
+* 24
+
+@Entity(name = "someThing") => this name will be used to identify the domain ..
+* this name will only be identified by hql queries ..ie ..name of the domain object
+
+@Table(name = "someThing") => this name will be used to which table referred by domain object
+* ..ie ..name of the table
+*
+* */
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+}

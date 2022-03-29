ea18e234d02f553bd90fbb02c3d85eacd6f544d6
gokulnb <gokul10cs12@gmail.com>
2022-03-30 00:30:10 +0530

    unit test.

--- src/test/java/com/example/hibernatelearn/HibernateLearnApplicationTests.java
+++ src/test/java/com/example/hibernatelearn/HibernateLearnApplicationTests.java
@@ -1,10 +1,24 @@
 package com.example.hibernatelearn;
 
+import com.example.hibernatelearn.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class HibernateLearnApplicationTests {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testBookRepository(){
+        long count = bookRepository.count();
+        assertThat(count).isGreaterThan(1);
+    }
+
 
     @Test
     void contextLoads() {

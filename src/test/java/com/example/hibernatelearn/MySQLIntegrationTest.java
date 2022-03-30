4e5bc02a633a432f74ca5218d2566833bec9915d
gokulnb <gokul10cs12@gmail.com>
2022-03-31 00:20:35 +0530

    mysql config changes.

--- src/test/java/com/example/hibernatelearn/MySQLIntegrationTest.java
+++ src/test/java/com/example/hibernatelearn/MySQLIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.example.hibernatelearn;
 
-import com.example.hibernatelearn.domain.Book;
 import com.example.hibernatelearn.repositories.BookRepository;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -10,35 +9,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class) //initialize the order
+@ActiveProfiles("local")
+/*
+* You assign profile to your components with @Profile; while testing select them with @ActiveProfiles,
+* while developing select them with spring.profiles.active property.
+* */
 @DataJpaTest //test splice only to test the db layer of the project, very minimal configuration
-@ComponentScan(basePackages = {"com.example.hibernatelearn.bootstrap"}) //add the componnent to the context
+@ComponentScan(basePackages = {"com.example.hibernatelearn.bootstrap"}) //add the component to the context
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // usually spring overrides the given profile
-
-public class SpringBootJpaTestSplice {
+//config with h2 configs we add autoconfigureTestDataBase to none, to avoid doing this by Spring.
+public class MySQLIntegrationTest {
 
     @Autowired
     BookRepository bookRepository;
 
-    @Commit //make the transaction persist after execution of the test
-    @Order(1) //test order
-    @Test
-    void testJpaTestSplice(){
-        long countBefore = bookRepository.count();
-        bookRepository.save(new Book("Name", "isbn", "hehe"));
-        assertThat(countBefore + 1).isEqualTo(3);
-    }
 
-    @Rollback(value = false) // not  rollback, make the transaction persist after execution of the test
-    @Order(2)
     @Test
-    void testJpaTestSpliceTransaction(){
+    void testMySQL(){
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(3);
+        assertThat(countBefore).isEqualTo(2);
     }
 }

7ae7131407bea78b690f5377d3f3253eaf43876a
gokulnb <gokul10cs12@gmail.com>
2022-04-04 23:54:44 +0530

    liquibase coonfig changes - for change log

--- src/test/java/com/example/hibernatelearn/MySQLIntegrationTest.java
+++ src/test/java/com/example/hibernatelearn/MySQLIntegrationTest.java
@@ -1,15 +1,11 @@
 package com.example.hibernatelearn;
 
 import com.example.hibernatelearn.repositories.BookRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;

be8c835678edefd207e73c776fb970bb28bb3cc9
gokulnb <gokul10cs12@gmail.com>
2022-04-12 00:12:56 +0530

    jdbc template - commit 1.

--- src/main/java/com/example/hibernatelearn/dao/AuthorDaoImpl.java
+++ src/main/java/com/example/hibernatelearn/dao/AuthorDaoImpl.java
@@ -1,180 +1,46 @@
 package com.example.hibernatelearn.dao;
 
 import com.example.hibernatelearn.domain.Author;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
-    private final DataSource dataSource;
-    Connection connection=null;
-    Statement statement= null;
-    PreparedStatement preparedStatement =null;
-    ResultSet resultSet = null;
 
+     private final JdbcTemplate jdbcTemplate;
 
-
-    public AuthorDaoImpl(DataSource dataSource) {
-
-        this.dataSource = dataSource;
+    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
+
 
     @Override
     public Author getById(Long id) {
-
-
-        try {
-            connection = dataSource.getConnection();
-            //changes for implementing prepared statement.
-            preparedStatement = connection.prepareStatement("SELECT  * FROM author where id =?");// this will cache
-            // the statement in to the connection to avoid identifying the query as new one each time. and use less cache.
-            // also the query compiled only once which improved the performance.
-
-            //bind the parameter
-            preparedStatement.setLong(1, id);
-//            resultSet = statement.executeQuery("SELECT  * FROM author where id =" + id);
-            resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()){
-                return getAuthorFromResult(resultSet);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-
-            try {
-                closeAll(resultSet, preparedStatement, connection);
-
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-
-        }
-        return null;
+        return jdbcTemplate.queryForObject("select * from author where id = ?", getRawMapper(), id);
     }
-
 
     @Override
     public Author getAuthorByName(String firstName, String lastName) {
-
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("SELECT  * FROM author where first_name =? OR last_name=?");// this will cache
-            preparedStatement.setString(1, firstName);
-            preparedStatement.setString(2, lastName);
-            resultSet = preparedStatement.executeQuery(); // execute query only for queries, not for updates.
-
-            if (resultSet.next()){
-                return getAuthorFromResult(resultSet);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(resultSet, preparedStatement, connection);
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-
-        }
         return null;
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("INSERT INTO author (first_name, last_name) values (?,?)");// this will cache
-            preparedStatement.setString(1, author.getFirstName());
-            preparedStatement.setString(2, author.getLastName());
-            preparedStatement.execute(); // execute() required for insertion.because
-            // we are not getting the result back. because of insertion.
-
-            Statement statement = connection.createStatement();
-
-            resultSet =statement.executeQuery("SELECT LAST_INSERT_ID()");// mySQL only query to get the last inserted id value
-
-            if (resultSet.next()){
-                Long savedId = resultSet.getLong(1);
-                return this.getById(savedId);
-            }
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(resultSet, preparedStatement, connection);
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-
-        }
         return null;
     }
 
     @Override
     public Author updateAuthor(Author author) {
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("UPDATE author set first_name =?, last_name=? where author.id=?");// this will cache
-            preparedStatement.setString(1, author.getFirstName());
-            preparedStatement.setString(2, author.getLastName());
-            preparedStatement.setLong(3, author.getId());
-            preparedStatement.execute(); // execute() required for update.because
-            // we are not getting the result back. because of update.
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(resultSet, preparedStatement, connection);
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
-        return this.getById(author.getId());
+        return null;
     }
 
     @Override
     public void deleteAuthorById(Long id) {
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("delete from author where id=?");// this will cache
-            preparedStatement.setLong(1, id);
-            preparedStatement.execute(); // execute() required for update.because
-            // we are not getting the result back. because of update.
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                closeAll(null, preparedStatement, connection);
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
     }
 
-    private Author getAuthorFromResult(ResultSet resultSet) throws SQLException {
-        Author author= new Author();
-        author.setId(resultSet.getLong("id"));
-        author.setFirstName(resultSet.getString("first_name"));
-        author.setLastName(resultSet.getString("last_name"));
-        return author;
-    }
-
-    private void closeAll(ResultSet resultSet, PreparedStatement preparedStatement, Connection connection) throws SQLException {
-        if (resultSet != null){
-            resultSet.close();
-        }
-        if (preparedStatement != null){
-            resultSet.close();
-        }
-        if (connection !=null){
-            connection.close();
-        }
+    private RowMapper<Author> getRawMapper(){
+        return new AuthorMapper();
     }
 }
--- src/test/java/com/example/hibernatelearn/AuthorDAOIntegrationTest.java
+++ src/test/java/com/example/hibernatelearn/AuthorDAOIntegrationTest.java
@@ -1,9 +1,7 @@
 package com.example.hibernatelearn;
 
 import com.example.hibernatelearn.dao.AuthorDao;
-import com.example.hibernatelearn.dao.BookDao;
 import com.example.hibernatelearn.domain.Author;
-import com.example.hibernatelearn.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -21,73 +19,6 @@ public class AuthorDAOIntegrationTest {
 
     @Autowired
     AuthorDao authorDao;
-
-    @Autowired
-    BookDao bookDao;
-
-    @Test
-    void testDeleteBook() {
-        Book book = new Book();
-        book.setIsbn("1234");
-        book.setPublisher("Self");
-        book.setTitle("my book");
-        Book saved = bookDao.saveNewBook(book);
-        bookDao.deleteBookById(saved.getId());
-        Book deleted = bookDao.getById(saved.getId());
-        assertThat(deleted).isNull();
-    }
-
-    @Test
-    void updateBookTest() {
-        Book book = new Book();
-        book.setIsbn("1234");
-        book.setPublisher("Self");
-        book.setTitle("my book");
-
-        Author author = new Author();
-        author.setId(3L);
-
-        book.setAuthor(author);
-        Book saved = bookDao.saveNewBook(book);
-
-        saved.setTitle("New Book");
-        bookDao.updateBook(saved);
-
-        Book fetched = bookDao.getById(saved.getId());
-
-        assertThat(fetched.getTitle()).isEqualTo("New Book");
-    }
-
-    @Test
-    void testSaveBook() {
-        Book book = new Book();
-        book.setIsbn("1234");
-        book.setPublisher("Self");
-        book.setTitle("my book");
-
-        Author author = new Author();
-        author.setId(3L);
-
-        book.setAuthor(author);
-        Book saved = bookDao.saveNewBook(book);
-
-        assertThat(saved).isNotNull();
-    }
-
-    @Test
-    void testGetBookByName() {
-        Book book = bookDao.findBookByTitle("Clean Code");
-
-        assertThat(book).isNotNull();
-    }
-
-    @Test
-    void testGetBook() {
-        Book book = bookDao.getById(3L);
-
-        assertThat(book.getId()).isNotNull();
-    }
-
 
     @Test
     void testDeleteAuthorById(){
